361eb49984dce77d6c52142300fa66319904695a
Zack <zack@zacharymaril.com>
2023-05-06 22:53:58 +0100

    importing old code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,31 @@
 use pyo3::prelude::*;
+//TODO: find a better exception to use here
+use pyo3::exceptions::PyBaseException;
+use ::jawohl as jw;
 
-/// Formats the sum of two numbers as string.
+
+/// Export complete_json to Python.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn complete_json(input: &str) -> PyResult<String> {
+    match jw::complete_json(input) {
+        Ok(s) => Ok(s),
+        Err(e) => Err(PyErr::new::<PyBaseException, _>(format!("{}", e))),
+    }
+} 
+
+/// Export get_closing_string_for_partial_json to Python.
+#[pyfunction]
+fn get_closing_string_for_partial_json(input: &str) -> PyResult<String> {
+    match jw::get_closing_string_for_partial_json(input) {
+        Ok(s) => Ok(s),
+        Err(e) => Err(PyErr::new::<PyBaseException, _>(format!("{}", e))),
+    }
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn jawohl(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(complete_json, m)?)?;
+    m.add_function(wrap_pyfunction!(get_closing_string_for_partial_json, m)?)?;
     Ok(())
 }
